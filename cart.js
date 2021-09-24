ac5d7e59e9c86e89cd6ecf2db53bec2ed8e032c6
Adam Mezővári <adam@Adams-MacBook-Pro.local>
2021-09-24 11:02:02 +0200

    restructure cart

--- cart.js
+++ cart.js
@@ -1,7 +1,8 @@
 // Cart class to store items and total price
 class Cart {
-    constructor(){
+    constructor(promotions){
         this.items = {}
+        this.promotions = promotions
         this.total = 0
     }
 
@@ -21,6 +22,70 @@ class Cart {
         this.total += item.price
     }
 
+    checkout(){
+
+        // mutually exclusive
+        let promotionApplied = false
+        
+        let singleSkuPromotions = this.promotions.filter(x=>x.isSingleSku)
+        let multiSkuPromotions = this.promotions.filter(x=>!x.isSingleSku)
+
+        // single SKU promotions
+        Object.keys(this.items).forEach(key => {
+
+            // find related promotions if any
+            let promotion = singleSkuPromotions.find(x=> x.skus[0] === key)
+    
+            // if no promotions found, check next sku
+            if (!promotion) return;
+    
+            // check if amount in cart is enough for promotion
+            if (this.items[key].amount >= promotion.skus.length) {
+                console.log("DISCOUNT! %d of %s for %d", promotion.skus.length, promotion.skus[0], promotion.price)
+                
+                let applyCounter = parseInt((this.items[promotion.skus[0]].amount - this.items[promotion.skus[0]].usedForPromotion) / promotion.skus.length)
+        
+                for (let i = 0; i<applyCounter;i++){
+                    this.items[promotion.skus[0]].usedForPromotion += promotion.skus.length
+                    this.total -= promotion.skus.length*this.items[promotion.skus[0]].unitPrice
+                    this.total += promotion.price
+                }
+                promotionApplied = true
+            }
+        })
+
+        if (promotionApplied) return
+
+        // mutli SKU promotions
+        multiSkuPromotions.forEach(promotion=> {
+        
+            // get list of available items for promotion
+            let availableItems = this.getAvailableItemsForPromotion()
+    
+            // check if all skus are in the cart for the promotion
+            let foundAllSku = true
+    
+            // check each sku against cart and available amount for discount
+            promotion.skus.forEach(sku =>{
+                if(!availableItems.includes(sku)){
+                    foundAllSku=false
+                }
+            });
+    
+            // if all skus are in the cart with enough amount
+            if (foundAllSku){
+                console.log("DISCOUNT! %s for %d", promotion.skus.join("+"), promotion.price)
+                promotion.skus.forEach(sku => {
+                    this.items[sku].usedForPromotion += 1
+                    this.total -= this.items[sku].unitPrice
+                })
+        
+                this.total += promotion.price
+            }
+        })
+        
+    }
+
     getAvailableItemsForPromotion(){
         let availableItems = []
         for (const [key, value] of Object.entries(this.items)) {
@@ -31,31 +96,31 @@ class Cart {
           return availableItems
     }
 
-    applySingleSkuPromotion(promotion){
-        if (!promotion) {
-            throw Error("Missing promotion");
-        }
-        let apply = parseInt((this.items[promotion.skus[0]].amount - this.items[promotion.skus[0]].usedForPromotion) / promotion.skus.length)
+    // applySingleSkuPromotion(promotion){
+    //     if (!promotion) {
+    //         throw Error("Missing promotion");
+    //     }
+    //     let apply = parseInt((this.items[promotion.skus[0]].amount - this.items[promotion.skus[0]].usedForPromotion) / promotion.skus.length)
         
-        for (let i = 0; i<apply;i++){
-            this.items[promotion.skus[0]].usedForPromotion += promotion.skus.length
-            this.total -= promotion.skus.length*this.items[promotion.skus[0]].unitPrice
-            this.total += promotion.price
-        }
+    //     for (let i = 0; i<apply;i++){
+    //         this.items[promotion.skus[0]].usedForPromotion += promotion.skus.length
+    //         this.total -= promotion.skus.length*this.items[promotion.skus[0]].unitPrice
+    //         this.total += promotion.price
+    //     }
         
-    }
+    // }
 
-    applyMultiSkuPromotion(promotion){
-        if (!promotion) {
-            throw Error("Missing promotion");
-        }
-        promotion.skus.forEach(sku => {
-            this.items[sku].usedForPromotion += 1
-            this.total -= this.items[sku].unitPrice
-        })
+    // applyMultiSkuPromotion(promotion){
+    //     if (!promotion) {
+    //         throw Error("Missing promotion");
+    //     }
+    //     promotion.skus.forEach(sku => {
+    //         this.items[sku].usedForPromotion += 1
+    //         this.total -= this.items[sku].unitPrice
+    //     })
 
-        this.total += promotion.price
-    }
+    //     this.total += promotion.price
+    // }
 
     printCart(){
         console.log("Items: ", this.items, "\nTotal: ",this.total+"\n")
--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const Item = require("./item")
 const Promotion = require("./promotion")
 const Cart = require("./cart")
 
+// Initialize active promotions
+let promotionAs = new Promotion(["A","A","A"],130)
+let promotionBs = new Promotion(["B","B"],45)
+let promotionCD = new Promotion(["C","D"],30)
+
+// Define shopping cart with active promotions
+let cart = new Cart([promotionAs,promotionBs,promotionCD])
+
 // Initialize Items
 let A = new Item("A",50);
 let B = new Item("B",30);
 let C = new Item("C",20);
 let D = new Item("D",15);
 
-// Initialize active promotions
-let promotionAs = new Promotion(["A","A","A"],130)
-let promotionBs = new Promotion(["B","B"],45)
-let promotionCD = new Promotion(["C","D"],30)
-let promotions = [promotionAs,promotionBs,promotionCD]
-
-// Define shopping cart
-let cart = new Cart()
 // add items to cart
 cart.addItem(A)
 cart.addItem(A)
@@ -34,56 +34,51 @@ cart.addItem(D)
 console.log("Your cart before applying promotions:")
 cart.printCart()
 
-let singleSkuPromotions = promotions.filter(x=>x.isSingleSku)
-let multiSkuPromotions = promotions.filter(x=>!x.isSingleSku)
+cart.checkout()
 
-console.log("Applying single sku promotions...")
-applySingleSkuPromotions(cart,singleSkuPromotions)
+// function applySingleSkuPromotions(cart, promotions){
 
-console.log("\nApplying multi sku promotions...")
-applyMultiSkuPromotions(cart,multiSkuPromotions)
+//     Object.keys(cart.items).forEach(key => {
 
-function applySingleSkuPromotions(cart, promotions){
+//         // find related promotions if any
+//         let promotion = promotions.find(x=> x.skus[0] === key)
 
-    Object.keys(cart.items).forEach(key => {
+//         // if no promotions found, check next sku
+//         if (!promotion) return;
 
-        // find related promotions if any
-        let promotion = promotions.find(x=> x.skus[0] === key)
+//         // check if amount in cart is enough for promotion
+//         if (cart.items[key].amount >= promotion.skus.length) {
+//             console.log("DISCOUNT! %d of %s for %d", promotion.skus.length, promotion.skus[0], promotion.price)
+//             cart.applySingleSkuPromotion(promotion)
+//             return true
+//         }
+//     })
+//     return false
+// }
 
-        // if no promotions found, check next sku
-        if (!promotion) return;
-
-        // check if amount in cart is enough for promotion
-        if (cart.items[key].amount >= promotion.skus.length) {
-            console.log("DISCOUNT! %d of %s for %d", promotion.skus.length, promotion.skus[0], promotion.price)
-            cart.applySingleSkuPromotion(promotion)
-        }
-    })
-}
-
-function applyMultiSkuPromotions(cart,promotions){
+// function applyMultiSkuPromotions(cart,promotions){
    
-    promotions.forEach(promotion=> {
+//     promotions.forEach(promotion=> {
         
-        // get list of available items for promotion
-        let availableItems = cart.getAvailableItemsForPromotion()
+//         // get list of available items for promotion
+//         let availableItems = cart.getAvailableItemsForPromotion()
 
-        // check if all skus are in the cart for the promotion
-        let foundAllSku = true
+//         // check if all skus are in the cart for the promotion
+//         let foundAllSku = true
 
-        // check each sku against cart and available amount for discount
-        promotion.skus.forEach(sku =>{
-            if(!availableItems.includes(sku)){
-                foundAllSku=false
-            }
-        });
+//         // check each sku against cart and available amount for discount
+//         promotion.skus.forEach(sku =>{
+//             if(!availableItems.includes(sku)){
+//                 foundAllSku=false
+//             }
+//         });
 
-        // if all skus are in the cart with enough amount
-        if (foundAllSku){
-            console.log("DISCOUNT! %s for %d", promotion.skus.join("+"), promotion.price)
-            cart.applyMultiSkuPromotion(promotion)
-        }
-    })
-}
+//         // if all skus are in the cart with enough amount
+//         if (foundAllSku){
+//             console.log("DISCOUNT! %s for %d", promotion.skus.join("+"), promotion.price)
+//             cart.applyMultiSkuPromotion(promotion)
+//         }
+//     })
+// }
 console.log("\nYour cart after applying promotions:")
 cart.printCart()
